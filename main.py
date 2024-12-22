d6d1502685e723244be6ef0dac8e06e15374cd62
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-12-22 15:23:12 +0300

    hotel payment

---
+++ main.py
@@ -0,0 +1,10 @@
+def hotel(bill,tip):
+    total =bill+tip
+    print("Total amount to be payed is",total)
+
+bill=int(input("Enter bill:"))
+tip=int(input("Enter tip:"))
+hotel(bill,tip)
+
+print("Thats too muuuuuuuuuuuuch!!!!!!!!!!!!!!!!!!!!!!!")
+
